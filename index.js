19571b6b6070a3d54501075029405c308cded6e0
Camden Van Sciver <camdenvs02@gmail.com>
2022-11-09 23:32:05 -0700

    added questions for user prompts

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = ['What is your project title?', 'What was your motivation for this project?', 'What problem does your project solve?', 'What did you learn in development?', 'How would you instruct someone to install this application?', 'How would a user use your application?', 'What are your contribution guidelines?', 'What tests did you run for your application? How can a user run those same tests?', 'What is your GitHub username?', 'What is your email?'];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
