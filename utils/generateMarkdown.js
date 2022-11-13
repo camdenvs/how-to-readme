55671b1f81b9c000300140cca8f0c8c383d347cd
Camden Van Sciver <camdenvs02@gmail.com>
2022-11-12 18:54:39 -0700

    added file and text generation

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,73 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// function that returns a license badge based on which license is passed in
+// If there is no license, returns an empty string
+function renderLicenseBadge(license) {
+  if (license == 'MIT') {
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  } else if (license == 'Apache') {
+    return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+  } else if (license == 'GPL v2') {
+    return '[![License: GPL v2](https://img.shields.io/badge/License-GPL_v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)'
+  } else {
+    return ''
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// function that returns the license link
+function renderLicenseLink(license) {
+  if (license == 'MIT') {
+    var url = 'https://opensource.org/licenses/MIT'
+  } else if (license == 'Apache') {
+    var url = 'https://opensource.org/licenses/Apache-2.0'
+  } else {
+    var url = 'https://opensource.org/licenses/GPL-2.0'
+  }
+  return url
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// function that returns the license section of README
+// If there is no license, returns 'N/A'
+function renderLicenseSection(license) {
+  if (license != 'None') {
+    return `${license} - ${renderLicenseLink(license)}`
+  } else {
+    return 'N/A'
+  }
+}
 
-// TODO: Create a function to generate markdown for README
+// function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+  ${renderLicenseBadge(data.license)}
+  ## Description
+  ${data.description}
 
-`;
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [License](#license)
+
+  ## Installation
+  ${data.install}
+
+  ## Usage
+  ${data.usage}
+
+  ## Contributing
+  ${data.contribution}
+
+  ## Tests
+  ${data.tests}
+  
+  ## License
+  ${renderLicenseSection(data.license)}
+
+  ## Questions
+  Reach out to me:
+  - GitHub: https://github.com/${data.github}
+  - Email: ${data.email}
+  `;
 }
 
 module.exports = generateMarkdown;
